524de6047f1eee8bea5ee95315668a75ceaccef0
Lapidis2 <hitlapidis2@gmail.com>
2025-04-05 00:14:32 +0200

    solving conflict in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,48 @@
-import React, { useState } from 'react'
+import React,{useState} from 'react'
 import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 import { IoLocationSharp, IoMail, IoCall } from "react-icons/io5";
 import { BsWhatsapp } from "react-icons/bs";
-import { Link } from "react-router-dom";
-
-
+import  axios from 'axios';
+ 
 const Footer = () => {
+	const [email, setEmail] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [message, setMessage] = useState('');
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setLoading(true);
+		try {
+			const res = await axios.post('https://easy-renting-bn.onrender.com/api/subscribe', {email});
+	
+			if (res.status === 201) {
+				setMessage(res.data.message);
+				setEmail('');
+
+			}
+			setLoading(false);
+		} catch (error) {
+			 if (error.response) {
+     
+      setMessage(error.response.data.message || 'Subscription failed. Please try again later.');
+    } else {
+      setMessage('Subscription failed. Please try again later.');
+    }
+    setTimeout(() => {
+      setMessage('');
+    }, 5000); 
+  } finally {
+    setLoading(false); 
+  }
+		  
+	}
+ 
   return (
 	<footer className="bg-[#002F47] text-white py-10 px-6 md:px-16">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         
         <div>
           <h2 className="text-2xl font-bold flex items-center">
-            <span className="text-white">GREAT CONNECTION BUSINESS GROUP</span>
+            <span className="text-green-700">GREAT CONNECTION BUSINESS GROUP</span>
           </h2>
           <p className="mt-4 text-gray-300 text-sm">
             Are you considering investing in real estate in Rwanda? great connection business group
@@ -28,11 +58,12 @@ const Footer = () => {
             Quick Links
           </h3>
           <ul className="mt-4 space-y-2 text-gray-300 text-sm">
-           <li> <Link to='/contact'>Contact</Link></li> 
-           <li>  <Link to='request-property'>Request Property</Link></li> 
-           <li> <Link to='supply-property'>Supply Property</Link></li> 
-           <li> <Link to='register'>Register</Link></li> 
-           <li> <Link to='login'>Login</Link></li> 
+            <li>About us</li>
+            <li>Neighborhoods</li>
+            <li>Blog</li>
+            <li>FAQs (Frequently Asked Questions)</li>
+            <li>Sell your property</li>
+            <li>Real Estate Investment Calculator</li>
           </ul>
         </div>
 
@@ -40,19 +71,28 @@ const Footer = () => {
         <div className="space-y-6">
           {/* Newsletter */}
           <div>
+			
             <h3 className="text-xl font-semibold border-b-2 border-green-700 pb-2">
               Newsletter
             </h3>
             <p className="text-sm text-gray-300 mt-2">Email address</p>
-            <input
+			<form action="" onSubmit={handleSubmit}>
+			<input
               type="email"
               placeholder="Enter your email"
               className="w-full px-3 py-2 mt-2 rounded-md bg-white text-gray-800"
+			  onChange={(e) => setEmail(e.target.value)}
             />
-            <button className="mt-2 w-full bg-green-400 border-green-700 text-white py-2 rounded-md
-			 hover:green-800 transition cursor-pointer ">
-              SUBSCRIBE
-            </button>
+            <button
+          type="submit"
+          className="mt-2 w-full bg-green-400 border-green-700 text-white py-2 rounded-md hover:bg-green-800 transition cursor-pointer"
+          
+        >
+         {loading ? 'Please wait!......' : 'Subscribe'}
+        </button>
+			</form>
+
+           {message && <h3 className="text-xs text-green-400 mt-1">{message}</h3>}
             <p className="text-xs text-gray-400 mt-1">We never spam you!</p>
           </div>
 
@@ -63,34 +103,15 @@ const Footer = () => {
             </h3>
             <p className="flex items-center mt-2 text-gray-300">
               <IoLocationSharp className="mr-2 border-green-700" />
-              <a 
-                href="https://www.google.com/maps?q=Nyarugenge,UR-CST" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="hover:text-green-500"
-              >
-                Nyarugenge, UR-CST
-              </a>
+              Ikaze House 1st Floor, Remera Kigali
             </p>
             <p className="flex items-center mt-2 text-gray-300">
               <IoMail className="mr-2 border-green-700" />
-              <a 
-                href="https://mail.google.com/mail/?view=cm&to=juniorally910@gmail.com" 
-                target="_blank" 
-                rel="noopener noreferrer"
-                className="hover:text-green-500"
-              >
-                juniorally910@gmail.com
-              </a>
+              business@quick.rw
             </p>
             <p className="flex items-center mt-2 text-gray-300">
               <IoCall className="mr-2 border-green-700" />
-              <a 
-                href="tel:+250785711348" 
-                className="hover:text-green-500"
-              >
-                +250 785 711 348
-              </a>
+              +250 785 711 348
             </p>
           </div>
         </div>
